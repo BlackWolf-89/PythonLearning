e39370fd74068a735293c917877bf67dacd575e8
BlackWolf89 <velkrish89@yahoo.in>
2021-01-02 10:01:04 +0000

    New Changes

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# with open('new.txt', mode='a') as my_file:
+#   my_file.write('This is new file \n')
+#   my_file.write('This is second line \n')
+
+tex = 'nujmber|test|range|value'
+print(tex.split('|')[2])
