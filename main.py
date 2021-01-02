6ae38ea5c2d373a03c88241291c982f6f09115b9
BlackWolf89 <velkrish89@yahoo.in>
2021-01-02 10:19:09 +0000

    Tanslator

--- main.py
+++ main.py
@@ -3,5 +3,16 @@
 #   my_file.write('This is new file \n')
 #   my_file.write('This is second line \n')
 
-tex = 'nujmber|test|range|value'
-print(tex.split('|')[2])
+# tex = 'nujmber|test|range|value'
+# print(tex.split('|')[2])
+
+from translate import Translator
+
+with open('test.txt', mode='r') as inputFile:
+  text1 = inputFile.read()
+  translator = Translator(to_lang = "es")
+  result = translator.translate(text1)
+  print(result)
+
+  with open('result.txt', mode='w') as outputFile:
+    outputFile.write(result)
